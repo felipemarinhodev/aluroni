d8d6c0a3749089c496f86616c5f63dbbc6651847
felipemarinhodev <felipmarinho@gmail.com>
2022-07-27 08:11:20 -0700

    refactor file name and imports

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -3,7 +3,7 @@ import styles from './Menu.module.scss';
 export default function Menu() {
   return (
     <main>
-      <nav className="menu"></nav>
+      <nav className={styles.menu}>Aluroni</nav>
     </main>
   );
 }
