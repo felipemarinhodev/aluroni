74e62c7f14a616d3084f7d99fc1ecc57f19f26fe
felipemarinhodev <felipmarinho@gmail.com>
2022-07-27 21:34:48 -0700

    add svg in the page

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -3,7 +3,9 @@ import styles from './Menu.module.scss';
 export default function Menu() {
   return (
     <main>
-      <nav className={styles.menu}>Aluroni</nav>
+      <nav className={styles.menu}>
+        <img src="../../assets/logo.svg" alt="logo do aluroni" />
+      </nav>
     </main>
   );
 }
