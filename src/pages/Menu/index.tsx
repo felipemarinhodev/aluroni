2330badde501b399d3c1dcbbbde6722a090f6286
felipemarinhodev <felipmarinho@gmail.com>
2022-07-29 06:52:15 -0700

    include component of search

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,7 +1,10 @@
-import styles from './Menu.module.scss';
+import { useState } from 'react';
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import styles from './Menu.module.scss';
+import SearchEngine from './SearchEngine';
 
 export default function Menu() {
+  const [search, setSearch] = useState('');
   return (
     <main>
       <nav className={styles.menu}>
@@ -10,6 +13,11 @@ export default function Menu() {
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código da massa!</div>
       </header>
+
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Menu</h3>
+        <SearchEngine search={search} setSearch={setSearch} />
+      </section>
     </main>
   );
 }
