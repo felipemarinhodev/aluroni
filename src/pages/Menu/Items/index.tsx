496996942b3d7a46ef61155a6d4ac3da68a077b5
felipemarinhodev <felipmarinho@gmail.com>
2022-08-01 21:02:25 -0700

    sort items in the list

--- src/pages/Menu/Items/index.tsx
+++ src/pages/Menu/Items/index.tsx
@@ -9,6 +9,8 @@ interface Props {
   sort: string;
 }
 
+type Items = typeof menu;
+
 export default function Items({ search, filter, sort }: Props) {
   const [list, setList] = useState(menu);
 
@@ -24,12 +26,25 @@ export default function Items({ search, filter, sort }: Props) {
     return true;
   }
 
+  function sortTo(list: Items): Items {
+    switch (sort) {
+      case 'porcao':
+        return list.sort((a, b) => (a.size > b.size ? 1 : -1));
+      case 'qtd_pessoas':
+        return list.sort((a, b) => (a.serving > b.serving ? 1 : -1));
+      case 'preco':
+        return list.sort((a, b) => (a.price > b.price ? 1 : -1));
+      default:
+        return list;
+    }
+  }
+
   useEffect(() => {
     const newList = menu.filter(
       (item) => testSearch(item.title) && testFilter(item.category.id)
     );
-    setList(newList);
-  }, [search, filter]);
+    setList(sortTo(newList));
+  }, [search, filter, sort]);
 
   return (
     <div className={styles.items}>
