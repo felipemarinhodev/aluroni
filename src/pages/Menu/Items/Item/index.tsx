b1e7e295e5c2bcc6183000d97182dabdb80fb84d
felipemarinhodev <felipmarinho@gmail.com>
2022-08-01 20:16:55 -0700

    change to get dynamically data

--- src/pages/Menu/Items/Item/index.tsx
+++ src/pages/Menu/Items/Item/index.tsx
@@ -1,21 +1,28 @@
 import logo from 'assets/logo.svg';
 import styles from './Item.module.scss';
+import menu from '../items.json';
 
-export default function Item() {
+type Props = typeof menu[0];
+
+export default function Item(props: Props) {
+  const { title, description, category, size, serving, price, photo } = props;
   return (
     <div className={styles.item}>
-      <div className={styles.item__imagem}>
-        <img src={logo} alt="image" />
+      <div className={styles.item__image}>
+        <img src={logo} alt={title} />
       </div>
-      <div className={styles.item__descricao}>
+      <div className={styles.item__description}>
         <div>
-          <h2>Macarrão</h2>
-          <p>Descrição Macarrão</p>
+          <h2>{title}</h2>
+          <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__porcao}>400g</div>
-          <div className={styles.item__qtdPessoas}>Serve 2 pessoas</div>
-          <div className={styles.item__valor}>R$ 50,00</div>
+          <div className={styles.item__type}>{category.label}</div>
+          <div className={styles.item__portion}>{size}g</div>
+          <div className={styles.item__amountPeople}>
+            Serve {serving} pessoa{serving === 1 ? '' : 's'}
+          </div>
+          <div className={styles.item__price}>R$ {price.toFixed(2)}</div>
         </div>
       </div>
     </div>
