8401033941ce0bcd1bd2fb9c7d1e2ee5a9a1a064
felipemarinhodev <felipmarinho@gmail.com>
2022-08-01 19:28:54 -0700

    open and close sort options

--- src/pages/Menu/Sort/index.tsx
+++ src/pages/Menu/Sort/index.tsx
@@ -1,11 +1,24 @@
-import styles from './Sort.module.scss';
+import classNames from 'classnames';
+import { useState } from 'react';
 import options from './options.json';
+import styles from './Sort.module.scss';
 
 export default function Sort() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <button className={styles.sort}>
+    <button
+      className={styles.sort}
+      onClick={() => setOpen(!open)}
+      onBlur={() => setOpen(false)}
+    >
       <span>Ordenar Por</span>
-      <div className={styles.sort__options}>
+      <div
+        className={classNames({
+          [styles.sort__options]: true,
+          [styles['sort__options--active']]: open,
+        })}
+      >
         {options.map((option) => (
           <div className={styles.sort__option} key={option.value}>
             {option.name}
